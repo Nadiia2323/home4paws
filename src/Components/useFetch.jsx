e9ddd788a26559075e389c45e8e3255e4b9250ae
Nadiia2323 <yariginanm@gmail.com>
2024-12-18 22:29:18 +0100

    useFetch changed

--- src/Components/useFetch.jsx
+++ src/Components/useFetch.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { useEffect } from "react";
 
 const useFetch = (url) => {
-  // console.log("url useFecth:>> ", url);
+  console.log("url useFecth:>> ", url);
   const [myData, setMyData] = useState({});
   const [isLoading, setIsLoading] = useState(true);
 
@@ -12,8 +12,20 @@ const useFetch = (url) => {
 
     var urlencoded = new URLSearchParams();
     urlencoded.append("grant_type", "client_credentials");
-    urlencoded.append("client_id", "ВАШ_CLIENT_ID");
-    urlencoded.append("client_secret", "ВАШ_CLIENT_SECRET");
+    urlencoded.append(
+      "client_id",
+      // VITE_CLIENT_ID
+      // "8chWq1iNwVsKezFDLmw8d4N93envu0j7Xa7dMljqH7mafph213"
+      // "ZXjyNDuuZAUVnf257xx4lj6PFYEv9T6MQl8A9iNOP3x47yUD3L"
+      "fRde8D5bc5TDoJZzRcXP6xQVbLA3oPZ0ruUa1zjgiN63uAHYCk"
+    );
+    urlencoded.append(
+      "client_secret",
+      // VITE_CLIENT_SECRET
+      // "zu5MOzBXJHYewOOrw9dZNT2x7dOd8R6BiIk1wO8U"
+      // "LVg9fTLFhFk01qFZK4ZY95aOm9ttjT4PX7x2qCas"
+      "qEuWZoQn2sVLhuCqaJBAH1Z0NAjObwUWbR5dQVJ7"
+    );
 
     var requestOptions = {
       method: "POST",
@@ -22,12 +34,12 @@ const useFetch = (url) => {
       redirect: "follow",
     };
 
-    return fetch("/api/v2/oauth2/token", requestOptions)
+    return fetch("https://api.petfinder.com/v2/oauth2/token", requestOptions)
       .then((response) => response.json())
       .then((result) => {
         return result.access_token;
       })
-      .catch((error) => console.error("Error fetching access token:", error));
+      .catch((error) => console.log("error", error));
   };
 
   const fetchData = async (access_token, url) => {
@@ -50,14 +62,14 @@ const useFetch = (url) => {
       if (accessToken) {
         try {
           const data = await fetchData(accessToken, url);
-          console.log("data :>> ", data);
+          // console.log("data :>> ", data);
           setMyData(data);
           setIsLoading(false);
         } catch (error) {
           console.log("error fetching Pet :>> ", error);
         }
       } else {
-        // alert("no access token");
+        alert("no access token");
       }
     } catch (error) {
       console.log("error getting access token :>> ", error);
--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,20 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-import { defineConfig } from 'vite';
-
+// https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://api.petfinder.com',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-    },
-  },
-});
+  plugins: [react()],
+})
+// import { defineConfig } from 'vite';
+
+// export default defineConfig({
+//   server: {
+//     proxy: {
+//       '/api': {
+//         target: 'https://api.petfinder.com',
+//         changeOrigin: true,
+//         rewrite: (path) => path.replace(/^\/api/, ''),
+//       },
+//     },
+//   },
+// });
